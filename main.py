7ce48c5a4c10a83a95ef58277df06896831e907c
delphine136tr <delphine136.tr@gmail.com>
2019-11-28 00:02:11 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,45 @@
+# Name:
+# Section: 
+# Date:
+# hw1.py
+
+##### Template for Homework 1, exercises 1.2-1.5 ######
+print("********** Exercise 1.1 **********")
+# Do your work for Exercise 1.1 here
+print("Hello World!")
+
+print("********** Exercise 1.2 **********")
+# Do your work for Exercise 1.2 here
+
+
+
+
+print("********** Exercise 1.3 **********")
+
+# Do your work for Excercise 1.3 here. Hint - how many different
+# variables will you need?
+
+
+
+print("********** Exercise 1.4 **********")
+print("********* Part II *************")
+# Do your work for Exercise 1.4.2 here
+
+
+print "********* Part III *************"
+# Do your work for Exercise 1.4.3 here
+
+
+
+print("********** Exercise 1.5 **********")
+# Do your work for Exercise 1.5 here
+
+
+print("********** Exercise 1.6 **********")
+# Do your work for Exercise 1.6 here
+
+print("********** Exercise 1.7 **********")
+# Do your work for Exercise 1.7 here
+
+print("********** Exercise 1.8 **********")
+# Do your work for Exercise 1.8 here
